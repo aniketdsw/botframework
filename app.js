c327096d893fb50daa1f870dddb49894a788d01b
Aniket Kumar <aniketdsw@gmail.com>
2017-03-13 14:37:29 +0530

    Changed App.js

--- app.js
+++ app.js
@@ -1,51 +1,45 @@
-
-var builder = require('botbuilder');
-var express = require('express');
+/*var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-var bot = new builder.UniversalBot(connector);
-
-
 app.get('/', function(req, res){
  res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', function(socket){
  socket.on('chat message', function(msg){
-     bot.dialog('/', function (msg) {
-    io.emit('chat message', msg);
-});
- //io.emit('chat message', mss());
- 
+ io.emit('chat message', msg);
  });
 });
 http.listen(port, function(){
  console.log('listening on ' + port);
+});*/
+
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
 });
-
-
-
-
-//function mss(msg){
-//var connector = new builder.ChatConnector({
-   // appId: process.env.MICROSOFT_APP_ID,
-  //  appPassword: process.env.MICROSOFT_APP_PASSWORD
-//});
-//var bot = new builder.UniversalBot(connector);
-//server.post('/', connector.listen());
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-
-
-//}
-
-
-
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
