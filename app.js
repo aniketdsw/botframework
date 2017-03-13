5b4c5bf05be3d499c4fba39c86f2569baccfca3f
Aniket Kumar <aniketdsw@gmail.com>
2017-03-13 11:31:03 +0530

    Changed App.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*var express = require('express');
+var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -15,43 +15,13 @@ http.listen(port, function(){
  console.log('listening on ' + port);
 });
 
-*/
 
 
 
-var restify = require('restify');
-var builder = require('botbuilder');
-
-//=========================================================
-// Bot Setup
-//=========================================================
-
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
-  //get index
 
 
-server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
-}));
 
 
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+
