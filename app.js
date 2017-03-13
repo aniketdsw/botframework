3f9602a6296eac3a70fcde47ba5e8be77dfc5648
Aniket Kumar <aniketdsw@gmail.com>
2017-03-13 13:42:06 +0530

    Changed App.js

--- app.js
+++ app.js
@@ -5,28 +5,19 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+
 app.get('/', function(req, res){
  res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', function(socket){
  socket.on('chat message', function(msg){
  
- io.emit('chat message', function (msg){
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-var bot = new builder.UniversalBot(connector);
-//server.post('/', connector.listen());
-
-//=========================================================
-// Bots Dialogs
-//=========================================================
-
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
-});
+ io.emit('chat message', mss());
  
 });
 });
@@ -37,7 +28,18 @@ http.listen(port, function(){
 
 
 
+function mss(){
+//server.post('/', connector.listen());
 
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
+
+}
 
 
 
